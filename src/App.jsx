3430383a7a3adeee84b4a452106f1f9467e014a1
WelBert-dev <83488863+WelBert-dev@users.noreply.github.com>
2022-07-11 23:30:43 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar";
+
+import './App.css'
+import { Outlet } from "react-router-dom";
+//import { MemoryRouter as Router } from 'react-router-dom';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar/>
+      <Outlet />
+
+    </div>
+  )
+}
+
+export default App;
